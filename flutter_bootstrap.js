fa17e6b7efd082c26e6aeb70768ad14cf7b3f11f
akram <125197025+akramfaeq@users.noreply.github.com>
2026-08-07 23:05:07 +0000

    Update app

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"0cd610717bde95fd88343c64f81c11ba4e5c00
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2631863872" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2308141295" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
